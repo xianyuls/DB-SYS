6e1def73805c38c756d3449cf6acb84f9915a4e2
14017 <14017@DESKTOP-JN84DCR>
2020-06-04 09:26:19 +0800

    添加了

--- sp05-eureka/src/main/java/cn/tedu/sp05/Aaa.java
+++ sp05-eureka/src/main/java/cn/tedu/sp05/Aaa.java
@@ -3,5 +3,8 @@ package cn.tedu.sp05;
 public class Aaa {
 	public static void main(String[] args) {
 		System.out.println("1111");
+		System.out.println("1111");
+		System.out.println("1111");
+		System.out.println("1111");
 	}
 }
